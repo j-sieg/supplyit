00156a0d76997cdf34b1fa09dcf012fbbceff51e
the-siegmeister <siegmeister@protonmail.com>
2021-03-20 05:28:07 +0800

    Simplify relationship declaration between Category and Product

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
-  has_many(:product_relationships,
-    class_name: 'CategoryProduct',
-    foreign_key: :category_id,
-    dependent: :destroy
-  )
-  has_many :products, through: :product_relationships
+  has_many :category_products, dependent: :destroy
+  has_many :products, through: :category_products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,6 @@
 class Product < ApplicationRecord
   validates_presence_of :name, :price, :location
 
-  has_many(:category_relationships,
-    class_name: 'CategoryProduct',
-    foreign_key: :product_id,
-    dependent: :destroy
-  )
-  has_many :categories, through: :category_relationships
+  has_many :category_products, dependent: :destroy
+  has_many :categories, through: :category_products
 end
