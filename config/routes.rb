f7a36f6301fb388665eddd2270a72f58f1c39b09
the-siegmeister <siegmeister@protonmail.com>
2021-03-22 00:13:37 +0800

    [GENERATED]: rails g controller LineItems create update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'line_items/create'
+  get 'line_items/update'
+  get 'line_items/destroy'
   root 'pages#index'
 
   devise_for :sellers
@@ -12,4 +15,5 @@ Rails.application.routes.draw do
 
   resources :carts, only: %i[destroy]
   resources :products, only: %i[index show]
+  resources :line_item, only: %i[create update destroy]
 end
