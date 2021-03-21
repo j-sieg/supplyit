365357c3f525b09773515c513436aee8bc7ba9d7
the-siegmeister <siegmeister@protonmail.com>
2021-03-21 16:36:55 +0800

    Style User registrations#edit and permit fields on account_update Remove outline from all form field inputs

--- app/models/user_parameter_sanitizer.rb
+++ app/models/user_parameter_sanitizer.rb
@@ -2,5 +2,6 @@ class UserParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
     permit(:sign_up, keys: [:full_name, :phone_number, :address, :birthday])
+    permit(:account_update, keys: [:full_name, :phone_number, :address, :birthday])
   end
 end
