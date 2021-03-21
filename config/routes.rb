efb79c3efc6c204640b155b4514aa9dbaf7adf30
the-siegmeister <siegmeister@protonmail.com>
2021-03-21 16:11:42 +0800

    Remove categories from controllers and routes along w/ its tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   devise_for :sellers
-  
+
   scope module: "sellers" do
     resources :products, except: %i[index show]
   end
 
-  resources :categories, except: %i[new]
   resources :products, only: %i[index show]
-
 end
