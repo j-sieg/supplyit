d32e5cf3ab9dca410ce0cd38d70ba27f7ee82403
the-siegmeister <siegmeister@protonmail.com>
2021-03-23 18:52:48 +0800

    Don't allow products that are referenced by line_items to be destroyed

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,15 @@ class Product < ApplicationRecord
   belongs_to :seller
   has_many :category_products, dependent: :destroy
   has_many :categories, through: :category_products
-  has_many :line_items, dependent: :destroy
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_line_items
+
+  private
+    def ensure_not_referenced_by_line_items
+      unless line_items.empty?
+        errors.add(:base, 'LineItems reference this product')
+        throw :abort
+      end
+    end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product with line items cannot be destroyed" do
+    product = products(:aggregate)
+    new_item = LineItem.create(product_id: product.id)
+    carts(:empty_cart).add_item(new_item)
+    new_item.save!
+
+    assert_no_difference('Product.count') do
+      product.destroy
+    end
+
+    assert_raise ActiveRecord::RecordNotDestroyed do
+      product.destroy!
+    end
+  end
 end
