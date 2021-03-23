9e5a6a2a984b620ffb6bbcddff088819fdd4ba4d
the-siegmeister <siegmeister@protonmail.com>
2021-03-24 04:38:09 +0800

    Mostly just styles Make the layout container and not container-fluid Make pink flash a "black  flash" get it? Add the flash to the new line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,10 +3,10 @@ class LineItemsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    new_item = @current_cart.add_item(product)
+    @new_item = @current_cart.add_item(product)
 
     respond_to do |format|
-      if new_item.save!
+      if @new_item.save!
         @added = true
         format.html { redirect_to products_url }
         format.turbo_stream do
