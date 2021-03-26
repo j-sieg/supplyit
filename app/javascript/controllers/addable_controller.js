1ea25e59c752ac3d1b50465d9363673f90216c72
the-siegmeister <siegmeister@protonmail.com>
2021-03-26 14:39:51 +0800

    Use stimulus controller to toggle classes on successful addition to cart Don't use turbo_stream to re-render the _product

---
+++ app/javascript/controllers/addable_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ['addButton']
+  static classes = ['added', 'failed']
+
+  initialize() {
+    this.addButtonTarget.addEventListener('turbo:submit-end', this.add.bind(this))
+  }
+
+  add(event) {
+    const pureElement = this.element
+    if (event.detail.success) {
+      this.element.classList.add(this.addedClass)
+    } else {
+      this.element.classList.add(this.failedClass)
+    }
+    this.element.parentNode.replaceChild(this.element, pureElement)
+  }
+
+  disconnect() {
+    this.addButtonTarget.removeEventListener('turbo:submit-end', this.add.bind(this))
+  }
+}
