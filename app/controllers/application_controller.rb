451a8e7ddc8aa8ffcc8f5d65220f6d362d98fad2
the-siegmeister <siegmeister@protonmail.com>
2021-03-26 14:37:25 +0800

    Customize redirects on login and logout Redirect devise resources to its products_url on login and to its login page on logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   include CustomDeviseParameterSanitizer
+
+  protected
+    def after_sign_in_path_for(resource)
+      return products_url if resource.is_a?(User)
+      sellers_products_url if resource.is_a?(Seller)
+    end
+
+    def after_sign_out_path_for(scope)
+      case scope
+      when :user
+        new_user_session_url
+      when :seller
+        new_seller_session_url
+      end
+    end
 end
