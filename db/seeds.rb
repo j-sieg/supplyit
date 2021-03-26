451e34a14a18bd1a162b010441616307a3b082b6
the-siegmeister <siegmeister@protonmail.com>
2021-03-27 04:33:55 +0800

    Center text for when there are no products Add development seeds that creates 10 products for each seller

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,17 @@
 ].each do |category_name|
   Category.first_or_create!(name: category_name)
 end
+
+if Rails.env.development?
+  Seller.find_each do |seller|
+    10.times do
+      seller.products.create!(
+        name: Faker::Construction.material,
+        price: Faker::Commerce.price(range: 500..10000),
+        location: Faker::TvShows::Simpsons.location,
+        categories: Category.order('RANDOM()').limit(3)
+      )
+    end
+  end
+  puts "Generated 10 products for each Seller"
+end
