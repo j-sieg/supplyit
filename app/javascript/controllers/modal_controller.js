583b482ebc22d1cf6fc79983a38a5dcc29c8791f
the-siegmeister <siegmeister@protonmail.com>
2021-04-02 00:31:16 +0800

    Replace data-confirm alerts with a bootstrap modal Use a stimulus controller to display failures on fail and dismiss the modal on submit Extract the dismissible alert that displays flash messages and use it on Products#index and Orders#index

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from 'stimulus'
+import { Modal } from 'bootstrap'
+
+export default class extends Controller {
+  static targets = ['body', 'submitForm', 'errorMessage']
+
+  connect() {
+    this.modal = new Modal(this.element)
+    if(this.hasSubmitFormTarget) {
+      this.submitFormTarget.addEventListener('turbo:submit-end', this.handleSubmit.bind(this))
+    }
+  }
+
+  handleSubmit({ detail }) {
+    const pureElement = this.element
+    if (detail.success) {
+      this.errorMessageTarget.classList.add('d-none')
+      this.bodyTarget.classList.remove('failed__submit')
+      this.modal.hide()
+    } else {
+      this.errorMessageTarget.classList.remove('d-none')
+      this.bodyTarget.classList.add('failed__submit')
+    }
+
+    this.element.parentNode.replaceChild(this.element, pureElement)
+  }
+
+  disconnect() {
+    if (this.hasSubmitFormTarget) {
+      this.submitFormTarget.removeEventListener('turbo:submit-end', this.handleSubmit.bind(this))
+    }
+  }
+}
