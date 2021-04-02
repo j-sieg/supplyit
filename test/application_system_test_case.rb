a42fa8c2f1cdcb45911206a90d7ad0c5f1f06b0f
the-siegmeister <siegmeister@protonmail.com>
2021-04-03 03:43:59 +0800

    Add User checkout flow system test This also adds helper functions for checking if line_item details and order details are on the DOM IDs are given to line_item and order for easier assertions when testing.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include ActionView::Helpers
+  include ApplicationHelper
   driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
 
   def seller_login_as(seller)
@@ -16,7 +18,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   end
 
   def user_login_as(user)
-    user =
+    email =
       case user when Symbol then users(user).email
       else user.email
       end
@@ -25,5 +27,27 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     fill_in "Email", with: email
     fill_in "Password", with: "1supersecret!"
     click_on "Log in"
+  end
+
+  def assert_line_item(line_item)
+    dom_id = "item_#{line_item.id}"
+    assert_selector 'tr', id: dom_id
+
+    within "##{dom_id}" do
+      assert_selector 'td', text: line_item.product.name
+      assert_selector 'td', text: line_item.price
+      assert_selector 'td', text: line_item.quantity
+    end
+  end
+
+  def assert_order_item(order)
+    dom_id = "order_#{order.id}"
+    assert_selector 'tr', id: dom_id
+    within "##{dom_id}" do
+      assert_selector 'td', text: "Order##{order.id}"
+      assert_selector 'td', text: formatted_time(order.created_at)
+      assert_selector 'td', text: order.status
+      assert_selector 'td', text: price_format(order.total_cost)
+    end
   end
 end
---
+++ test/system/user_checkout.rb
@@ -0,0 +1,44 @@
+require "application_system_test_case"
+
+class UserCheckout < ApplicationSystemTestCase
+  setup do
+    @user = users(:dane)
+  end
+
+  test "user loads cart, checks out, and orders" do
+    user_login_as(@user)
+    within "#current-cart-details" do
+      assert_no_selector "a", text: "Checkout"
+    end
+
+    products = [products(:aggregate), products(:cement)]
+    products.each do |product|
+      within "article#product_#{product.id}" do
+        assert_difference 'LineItem.count' do
+          2.times { click_on 'add to cart' }
+        end
+      end
+    end
+
+    assert_equal 2, @user.cart.line_items.count
+
+    @user.cart.line_items.each do |line_item|
+      within '#current-cart-details' do
+        assert_line_item(line_item)
+      end
+    end
+
+    within "#current-cart-details" do
+      assert_selector "a", text: "Checkout"
+      click_on "Checkout"
+    end
+
+    click_on "Pay"
+    assert_difference '@user.orders.count' do
+      click_on "Confirm"
+    end
+
+    assert_text "Successfully created your order"
+    assert_order_item(@user.orders.last)
+  end
+end
