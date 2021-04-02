8a589e3bda5a9d39f529cc1cae87790078f5c129
the-siegmeister <siegmeister@protonmail.com>
2021-04-03 05:17:39 +0800

    Only include ActionView::Helpers::NumberHelper in ApplicationSystemTestCase Including ActionView::Helpers was failing the Sellers::ProductsTest because it overrides the select method from ApplicationSystemTestCase so we only `include` the NumberHelper because we have tests that use `number_to_currency`.
    I also removed the datasets in the seller's `_product` because it
    doesn't need the stimulus controller.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  include ActionView::Helpers
+  include ActionView::Helpers::NumberHelper
   include ApplicationHelper
   driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
 
