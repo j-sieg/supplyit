5d7bdeb394a8d756282bc7ae00ab416acccfcc28
the-siegmeister <siegmeister@protonmail.com>
2021-04-02 18:38:40 +0800

    Add styles for when the cart is open. Customize scrollbar

--- app/javascript/controllers/cart_details_controller.js
+++ app/javascript/controllers/cart_details_controller.js
@@ -1,14 +1,18 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static classes = ['open']
+
   connect() {
     this.retainer = this.element.parentElement
     const opened = this.retainer.dataset.cartOpened === 'true'
 
     if (opened) {
       this.element.setAttribute('open', true)
+      this.element.classList.add(this.openClass)
     } else {
       this.element.removeAttribute('open')
+      this.element.classList.remove(this.openClass)
     }
   }
 
@@ -16,8 +20,10 @@ export default class extends Controller {
     const open = !this.element.hasAttribute('open')
     if (open) {
       this.retainer.dataset.cartOpened = true
+      this.element.classList.add(this.openClass)
     } else {
       this.retainer.dataset.cartOpened = false
+      this.element.classList.remove(this.openClass)
     }
   }
 }
